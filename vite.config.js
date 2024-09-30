2239afbf480c8ec46f58f8b82ebb4179f76a00e9
municipalidadfernandez <municipalidadfernandez@gmail.com>
2024-09-30 10:34:58 -0300

    Implementando plugin para pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Escuela Técnica N°12',
+        short_name: 'ET12',
+        description: 'App Oficial de Escuela Técnica N°12 ',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/logo-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/logo-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+
+      // Esta opción asegura que se generen los archivos
+      devOptions: {
+        enabled: true, // Habilita el plugin también en desarrollo
+      },
+    }),
+  ],
+});
