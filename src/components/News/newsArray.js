f5ec023a7fafb220d0b404ec0d7595e56acfbb82
municipalidadfernandez <municipalidadfernandez@gmail.com>
2024-12-18 21:59:54 -0300

    Agregando noticias actos de colacion 2024

--- src/components/News/newsArray.js
+++ src/components/News/newsArray.js
@@ -1,6 +1,42 @@
 export const posts = [
-    {
+
+      {
       id: 1,
+      title: "Emotivo acto de egreso en la Escuela Técnica de Fernández",
+      href: "https://elprogresoweb.com.ar/2024/12/13/emotivo-acto-de-egreso-en-la-escuela-tecnica-de-fernandez/",
+      description:
+        "Este jueves por la noche tuvo lugar el acto de egresados de los alumnos de la escuela Técnica Nº 12, el cual estuvo encabezado por el rector Prof. Tito Livio Manfredi junto a el cuerpo docente de la institución.",
+      date: "12 de Diciembre, 2024",
+      datetime: "2024-12-12",
+      category: { title: "Noticias", href: "#" },
+      author: {
+        name: "Escuela Técnica Nº12",
+        role: "Admin",
+        href: "#",
+        imageUrl: "https://scontent-eze1-1.xx.fbcdn.net/v/t39.30808-6/470202715_992797869558078_7827651955204417838_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_ohc=HwOgdxuw9g0Q7kNvgGpA-Fl&_nc_oc=AdhwFaxhnfSPcXKjfQZLX3tcLN0nCiBINyrafhVDFoNyyXF2duVv-HjVGzGqgtAzYrwPbC45FAXr2iNsef57-YZl&_nc_zt=23&_nc_ht=scontent-eze1-1.xx&_nc_gid=A6W2OQhBdIA4-QZRK4CRbgZ&oh=00_AYBAnRSlLzCKGfn3CbW_h0w20bsZdmyDJPqQLBcYZyHQ4g&oe=6769373F",
+      },
+  },
+    
+
+   {
+      id: 2,
+      title: "Mejor promedio: En la escuela Técnica el premio UCSE fue para Santiago Sebastián Díaz Torres",
+      href: "https://elprogresoweb.com.ar/2024/12/14/mejor-promedio-en-la-escuela-tecnica-el-premio-ucse-fue-para-santiago-sebastian-diaz-torres/",
+      description:
+        "En la oportunidad se le hizo entrega del premio UCSE de la Universidad Católica de Santiago del Estero al alumno Santiago Sebastián Díaz Torres como reconocimiento al mérito en su proceso de formación académica secundaria.",
+      date: "12 de Diciembre, 2024",
+      datetime: "2024-12-12",
+      category: { title: "Noticias", href: "#" },
+      author: {
+        name: "Escuela Técnica Nº12",
+        role: "Admin",
+        href: "#",
+        imageUrl: "https://elprogresoweb.com.ar/wp-content/uploads/2024/12/Egreso-Tecnica12-2024-12-12-116.jpg",
+      },
+    },
+    
+    {
+      id: 3,
       title: "Día del maestro: Araujo participó del acto homenaje a Domingo Faustino Sarmiento",
       href: "https://elprogresoweb.com.ar/2024/09/11/dia-del-maestro-araujo-participo-del-acto-homenaje-a-domingo-faustino-sarmiento/",
       description:
@@ -17,7 +53,7 @@ export const posts = [
     },
   
     {
-      id: 2,
+      id: 4,
       title: "Alumnos de tercer año de la Escuela Técnica Nº 12 realizaron la promesa de lealtad a la Constitución Nacional",
       href: "https://elprogresoweb.com.ar/2024/05/06/alumnos-de-tercer-ano-de-la-escuela-tecnica-no-12-realizaron-la-promesa-de-lealtad-a-la-constitucion-nacional/",
       description:
@@ -34,7 +70,7 @@ export const posts = [
     },
   
     {
-      id: 3,
+      id: 5,
       title: "Alumnos abanderados de sexto año de la Escuela Técnica Nº 12",
       href: "https://elprogresoweb.com.ar/wp-content/uploads/2024/05/IMG-20240502-WA0178.jpg",
       description:
